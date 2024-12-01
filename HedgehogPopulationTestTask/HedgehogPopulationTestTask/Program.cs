ea60b7f8b56f52f55c7c3a090e98f7d35f1d7c5d
Chizh-lx <62302984+Chizh-lx@users.noreply.github.com>
2024-12-01 21:39:22 +0200

    Update
    Some fixes and code improves

--- HedgehogPopulationTestTask/HedgehogPopulationTestTask/Program.cs
+++ HedgehogPopulationTestTask/HedgehogPopulationTestTask/Program.cs
@@ -1,5 +1,5 @@
-﻿int[] hedgehogPopulation = { 2, 6, 6 }; 
-int targetColor = 0;        
+﻿int[] hedgehogPopulation = { 0, 0, 6 };
+int targetColor = 1;
 
 int result = MinMeetings(hedgehogPopulation, targetColor);
 
@@ -11,43 +11,53 @@ static int MinMeetings(int[] population, int targetColor)
     int firstNonTarget = (targetColor + 1) % 3;
     int secondNonTarget = (targetColor + 2) % 3;
 
+    int nullCounter = 0;
     int steps = 0;
 
-    if ((population[firstNonTarget] + population[secondNonTarget]) % 3 != 0)
+    if ((population[firstNonTarget] - population[secondNonTarget]) % 3 != 0)
     {
         return -1;
     }
 
+    foreach (int x in population)
+    {
+        if (x == 0)
+        {
+            nullCounter++;
+        }
+
+        if (nullCounter == 2)
+        {
+            return -1;
+        }
+    }
+
+
     while (population[targetColor] != targetSum)
     {
+        steps++;
+
         if (population[firstNonTarget] > 0 && population[secondNonTarget] > 0)
         {
             population[firstNonTarget]--;
             population[secondNonTarget]--;
-            population[targetColor] = population[targetColor] + 2;
-            steps++;
+            population[targetColor] += 2;
+            continue;
         }
-        else
+
+        if (population[firstNonTarget] > 0)
         {
-            if (population[firstNonTarget] > 0)
-            {
-                population[firstNonTarget]--;
-                population[targetColor]--;
-                population[secondNonTarget] += 2;
-                steps++;
-            }
-            else if (population[secondNonTarget] > 0)
-            {
-                population[secondNonTarget]--;
-                population[targetColor]--;
-                population[firstNonTarget] += 2;
-                steps++;
-            }
-            else
-            {
-               return -1;
-            }
+            population[firstNonTarget]--;
+            population[targetColor]--;
+            population[secondNonTarget] += 2;
+            continue;
         }
+
+        population[secondNonTarget]--;
+        population[targetColor]--;
+        population[firstNonTarget] += 2;
+        continue;
+
     }
 
     return steps;
