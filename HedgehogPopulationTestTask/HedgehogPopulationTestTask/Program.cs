3e4064a7b1806d20cc4f39a27c12e095f52821f9
Chizh-lx <62302984+Chizh-lx@users.noreply.github.com>
2024-12-01 18:43:06 +0200

    Test tasks
    Here is 2 test tasks for technical review stage for Admixer (Technical support pos)

---
+++ HedgehogPopulationTestTask/HedgehogPopulationTestTask/Program.cs
@@ -0,0 +1,55 @@
+﻿int[] hedgehogPopulation = { 2, 6, 6 }; 
+int targetColor = 0;        
+
+int result = MinMeetings(hedgehogPopulation, targetColor);
+
+Console.WriteLine(result);
+
+static int MinMeetings(int[] population, int targetColor)
+{
+    int targetSum = population.Sum();
+    int firstNonTarget = (targetColor + 1) % 3;
+    int secondNonTarget = (targetColor + 2) % 3;
+
+    int steps = 0;
+
+    if ((population[firstNonTarget] + population[secondNonTarget]) % 3 != 0)
+    {
+        return -1;
+    }
+
+    while (population[targetColor] != targetSum)
+    {
+        if (population[firstNonTarget] > 0 && population[secondNonTarget] > 0)
+        {
+            population[firstNonTarget]--;
+            population[secondNonTarget]--;
+            population[targetColor] = population[targetColor] + 2;
+            steps++;
+        }
+        else
+        {
+            if (population[firstNonTarget] > 0)
+            {
+                population[firstNonTarget]--;
+                population[targetColor]--;
+                population[secondNonTarget] += 2;
+                steps++;
+            }
+            else if (population[secondNonTarget] > 0)
+            {
+                population[secondNonTarget]--;
+                population[targetColor]--;
+                population[firstNonTarget] += 2;
+                steps++;
+            }
+            else
+            {
+               return -1;
+            }
+        }
+    }
+
+    return steps;
+}
+
